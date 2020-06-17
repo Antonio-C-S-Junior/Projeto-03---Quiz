284d6f0304ef4b2ee4af06cca6dfa153f491979c
Antonio <>
2020-06-17 08:52:06 -0300

    Adicionado BD ao projeto

--- src/java/db/User.java
+++ src/java/db/User.java
@@ -5,11 +5,40 @@
  */
 package db;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 56235
  */
 public class User {
-    String login;
-    String name;
+    private String login;
+    private String name;
+    
+    public ArrayList<User> getList() throws Exception{
+        ArrayList<User> list = new ArrayList<>();
+        
+        return list;
+    }
+
+    public User(String login, String name) {
+        this.login = login;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }
